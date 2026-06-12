0e6bb83cf3f4d85d9567441a12b49945989e44a7
Vex <hexavexagon@gmail.com>
2026-06-12 17:40:52 +1200

    NNN.js improvements

--- Projects/NNN/nnn.js
+++ Projects/NNN/nnn.js
@@ -6,16 +6,17 @@
 
 convert = () => {
   let input = document.getElementById('nnn-input').value;
-  console.log('value is: ', input);
   let result = nullify(input);
   document.getElementById('nnn-result').textContent = result;
 }
 
 nullify = (input) => {
+  let timer = Date.now();
   let resultMid = [];
   for (let i = 0; i < input; i++) {
-    // TODO remember how the fuck this works at some point
     resultMid.push(`{${resultMid.join(', ')}}`);
   }
+  // It turns out this timer is pretty useless because the function is too fast but oh well
+  document.getElementById('js-timer').textContent = Date.now() - timer;
   return `|{${resultMid.join(', ')}}|`;
 }
