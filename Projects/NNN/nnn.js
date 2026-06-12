97136b9936709e685448e0b0b19f296d26a1ff27
Vex <hexavexagon@gmail.com>
2026-06-12 18:05:53 +1200

    remove timer

--- Projects/NNN/nnn.js
+++ Projects/NNN/nnn.js
@@ -1,9 +1,3 @@
-// grab #nnn-input.value
-// convert it
-// print result to #nnn-result.text
-
-// maybe time the function so nnn across different languages can be compared
-
 convert = () => {
   let input = document.getElementById('nnn-input').value;
   let result = nullify(input);
@@ -11,12 +5,9 @@ convert = () => {
 }
 
 nullify = (input) => {
-  let timer = Date.now();
   let resultMid = [];
   for (let i = 0; i < input; i++) {
     resultMid.push(`{${resultMid.join(', ')}}`);
   }
-  // It turns out this timer is pretty useless because the function is too fast but oh well
-  document.getElementById('js-timer').textContent = Date.now() - timer;
   return `|{${resultMid.join(', ')}}|`;
 }
