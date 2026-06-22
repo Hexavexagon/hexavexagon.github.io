8f29fb223c1afefcdf0e832d760966f591cb8437
Vex <hexavexagon@gmail.com>
2026-06-22 16:39:43 +1200

    breakMe checkbox WIP

--- Projects/NNN/nnn.js
+++ Projects/NNN/nnn.js
@@ -5,6 +5,11 @@ convert = () => {
 }
 
 nullify = (input) => {
+  let breakMe = document.getElementById('breakMe').value === 'on';
+  console.log('bre', breakMe);
+  if (input > 21 && !breakMe) {
+    return 'Specified number too high';
+  }
   let resultMid = [];
   for (let i = 0; i < input; i++) {
     resultMid.push(`{${resultMid.join(', ')}}`);
